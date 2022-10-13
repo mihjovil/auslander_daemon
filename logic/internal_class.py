224e30e4db11f93c5879451783d5add1d7feb00c
mihjovil <ma.caldas331@gmail.com>
2022-10-13 07:12:28 +0200

    :art: change ENV to capital names

--- logic/internal_class.py
+++ logic/internal_class.py
@@ -6,9 +6,9 @@ import time
 
 # region variables
 auslander_path = "https://auslaenderbehoerdeonline.hannover-stadt.de/index.cfm?CFID=1191255&CFTOKEN=ad7fa839fad70672-EEFB617A-A3C7-DC0D-6B61823C37AC16EC"
-token = os.getenv("token")
+token = os.getenv("TOKEN")
 bot_url = f"https://api.telegram.org/bot{token}"
-params = {"chat_id": os.getenv("chat_id"),
+params = {"chat_id": os.getenv("CHAT_ID"),
           "text": f'There seems to be appointments in the Ausländerbehörde. <a href="{auslander_path}">Click here to check!</a>'}
 pattern = r".*(Bitte versuchen Sie es am).*( Montag erneut).*"
 # endregion
