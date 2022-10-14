9cd62361649495ee7264cec9a4fb1958f4d04eeb
mihjovil <ma.caldas331@gmail.com>
2022-10-14 15:05:46 +0200

    :sparkles: added daemon to visit the website every minute

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from flask import Flask
-from multiprocessing import Queue, Process
 from logic.internal_class import check_website
+from multiprocessing import Process
+import json
+
 
 def create_app():
     app = Flask(__name__)
-    
+    # Variables
+    daemon_status_file = "static/daemon_status.json"
+
+    # region Daemon declaration
+    process = Process(target=check_website)
+    process.start()
+    # endregion
+
     @app.route('/')
     def index():
         return "<h1>Index page</h1><p>This is not an app with user interface.</p>"
-        
 
-    if __name__ == '__main__':
-        app.run()
+    @app.route("/switch_status", methods=["GET"])
+    def change_daemon_status():
+        # Read the current status
+        with open(daemon_status_file, "r") as f:
+            status = json.load(f)
+        current_status = status["on"]
+        # Change the status
+        with open(daemon_status_file, "w") as f:
+            json.dump({"on": not current_status}, f)
+        return f"<h1>The Daemon status has been changed to {current_status}</h1>"
+
     return app
